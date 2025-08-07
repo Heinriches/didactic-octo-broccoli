09cff28f6e2d820bba166951f25fde99e4cd4226
Heinriches <179625706+Heinriches@users.noreply.github.com>
2025-08-06 22:32:53 -0300

    05 - Final commit

---
+++ 05/PrimeirosPassos/src/PrimeirosPassosJava/OlaMundo.java
@@ -0,0 +1,24 @@
+//package - Pasta do Projeto
+package PrimeirosPassosJava;
+
+//Todo progrma JAVA é uma classe/
+//O nome da classe é o mesmo nome do arquivo
+//public - Publico
+//class - Classe
+public class OlaMundo {
+	
+	//Precisamos criar um método
+	//public - Público
+	//static - Estático
+	//void - Retorno vazio
+	//main - Nome do método ele é o principal
+	//String - Texto Array
+	//args - Nome do array
+	public static void main(String[] args) {
+		
+		//System.out - é o objeto de saída padrão
+		//print - método de impressão
+		System.out.println("Olá, mundo!");
+	}
+
+}
