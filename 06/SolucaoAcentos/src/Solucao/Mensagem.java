e52912b9286e9300e31d5e5c0fa060d144a0c20c
Heinriches <179625706+Heinriches@users.noreply.github.com>
2025-08-06 22:49:19 -0300

    06 - Final commit

---
+++ 06/SolucaoAcentos/src/Solucao/Mensagem.java
@@ -0,0 +1,10 @@
+package Solucao;
+
+public class Mensagem {
+	public static void main(String[] args) {
+		System.out.println("Olá, mundo!");
+		System.out.println("Gol do Pelé!");
+		System.out.println("ááá ééé ííí óóó úúú");
+	}
+
+}
